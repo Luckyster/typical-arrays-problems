bd924d47f15c36b76e463495be2e00441def0a24
Luckyster <gleb.phrolow@gmail.com>
2020-03-12 18:00:23 +0200

    feat: add issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  let m = 0;
+  if(array == undefined || array.length == 0){
+      return 0;
+  }
+  for(let i = 0; i < array.length; i++){
+      if(i == 0){
+          m = array[0];
+      } else if(array[i] < m){
+          m = array[i];
+      }
+  }
+  return m;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let m = 0;
+    if(array == undefined || array.length == 0){
+        return 0;
+    }
+    for(let i = 0; i < array.length; i++){
+        if(i == 0){
+            m = array[0];
+        } else if(array[i] > m){
+            m = array[i];
+        }
+    }
+    return m;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let a = 0;
+    if(array == undefined || array.length == 0){
+        return 0;
+    }
+    for(let i = 0; i < array.length; i++){
+            a += array[i];
+    }
+     a = a / (array.length) * 100;
+    console.log(Math.floor(a));
+    return Math.floor(a) / 100;
 }
